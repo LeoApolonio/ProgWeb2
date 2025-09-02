449bbe0834293b3050fb215a3ad6b62230a6891e
LeoApolonio <leonardoapolonio9@gmail.com>
2025-09-02 09:47:21 -0600

    Trabajo de clase 02 09 2025 - V1

---
+++ TrabajoClase/02092025/app4.js
@@ -0,0 +1,8 @@
+let fruits = ["Manzana", "Platano", "Pera"];
+
+console.log(frutas);
+console.log(frutas.length);
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
---
+++ TrabajoClase/02092025/app5.js
@@ -0,0 +1,6 @@
+let fruits = ["Manzana", "Platano", "Pera", "Fresa","Mandarina"];
+
+for( let i = 0; i < fruits.length ; i++)
+    {
+        console.log(fruits[i]);
+    }
